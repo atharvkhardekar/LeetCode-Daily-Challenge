0a058c18abc8bdea345faa46c85fa3d455754488
atharvkhardekar <khardekaratharv@gmail.com>
2024-12-03 22:22:31 +0530

    03/12/2024 - LeetCode(2109)

---
+++ String/AddingSpacesToString.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        int m=s.size();
+        int n=spaces.size();
+
+        string result;
+        int i=0;
+        int j=0;
+
+        while(i<m){
+            if(j<n and i == spaces[j]){
+                result.push_back(' ');
+                j++;
+            }
+            result.push_back(s[i]);
+            i++;
+        }
+        return result;
+    }
+};
