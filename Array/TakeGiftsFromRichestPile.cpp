ef1ff3a6de74ff955346774244c6a671fc8a1059
atharvkhardekar <khardekaratharv@gmail.com>
2024-12-12 22:01:48 +0530

    12/12/2024 - LeetCode(2558)

---
+++ Array/TakeGiftsFromRichestPile.cpp
@@ -0,0 +1,23 @@
+// LeetCode Problem No. 2558
+// Daily Challenge - 12/12/2024
+
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> maxHeap(gifts.begin(), gifts.end());
+        
+        for (int i = 0; i < k; i++) {
+            int maxGifts = maxHeap.top(); 
+            maxHeap.pop(); 
+            int remainingGifts = floor(sqrt(maxGifts)); 
+            maxHeap.push(remainingGifts); 
+        }
+        
+        long long totalGifts = 0;
+        while (!maxHeap.empty()) {
+            totalGifts += maxHeap.top();
+            maxHeap.pop();
+        }
+        return totalGifts;
+    }
+};
