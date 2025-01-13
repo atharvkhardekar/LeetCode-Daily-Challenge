5aad9a37ddb839a23cc71741daa30ae76ff3a750
atharvkhardekar <khardekaratharv@gmail.com>
2025-01-13 20:07:59 +0530

    13/01/2025 - LeetCode(3223)

---
+++ String/MinimumLengthOfStringAfterOperations.cpp
@@ -0,0 +1,23 @@
+// LeetCode Problem No. 3223
+// Daily Challenge - 13/01/2025
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> freq(26, 0); 
+        
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+        
+        int length = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] & 1) {
+                length += 1;
+            } else {
+                length += min(2, freq[i]);
+            }
+        }
+        return length;
+    }
+};
