0a07234d24ce26ac69303d0dc609603e64e3b4bb
atharvkhardekar <khardekaratharv@gmail.com>
2025-02-02 22:46:02 +0530

    02/02/2025 - LeetCode(1752)

---
+++ Array/(Feb 2)_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,17 @@
+// LeetCode Problem No. 1752
+// Daily Challenge - 02/02/2025
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+};
