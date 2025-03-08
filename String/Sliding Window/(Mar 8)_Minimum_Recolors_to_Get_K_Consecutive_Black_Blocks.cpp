f9509d9e9890c738ab7db18d267222222a1c3c92
atharvkhardekar <khardekaratharv@gmail.com>
2025-03-08 16:08:42 +0530

    08/03/2025 - LeetCode(2379)

---
+++ String/Sliding Window/(Mar 8)_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
@@ -0,0 +1,28 @@
+// LeetCode Problem No. 2379
+// Daily Challenge - 08/03/2025
+
+class Solution {
+    public:
+        int minimumRecolors(string blocks, int k) {
+            int n = blocks.size();
+            int minOperations = k; 
+            int whiteCount = 0;
+    
+            for (int i = 0; i < k; i++) {
+                if (blocks[i] == 'W') {
+                    whiteCount++;
+                }
+            }
+            minOperations = whiteCount;
+    
+            for (int i = k; i < n; i++) {
+                if (blocks[i - k] == 'W') whiteCount--; 
+                if (blocks[i] == 'W') whiteCount++; 
+    
+                minOperations = min(minOperations, whiteCount);
+            }
+    
+            return minOperations;
+        }
+};
+    
