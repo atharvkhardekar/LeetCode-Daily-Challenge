0a549fe1cabdd6ae99dae5d3b2310a075fc1556f
atharvkhardekar <khardekaratharv@gmail.com>
2025-05-15 22:50:09 +0530

    15/05/2025 - LeetCode(2900)

---
+++ Array/Greedy/(May 15)_Longest_Unequal_Adjacent_Groups_Subsequence_I.cpp
@@ -0,0 +1,23 @@
+// LeetCode Problem No. 2900
+// Daily Challenge - 15/05/2025
+
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<string> result;
+
+        if (words.empty()) return result;
+
+        result.push_back(words[0]);
+        int lastGroup = groups[0];
+
+        for (int i = 1; i < words.size(); ++i) {
+            if (groups[i] != lastGroup) {
+                result.push_back(words[i]);
+                lastGroup = groups[i];
+            }
+        }
+
+        return result;
+    }
+};
