799be4daef3d55ea22d3b6f6827056adf8797816
atharvkhardekar <khardekaratharv@gmail.com>
2025-06-13 16:39:39 +0530

    13/06/2025 - LeetCode(2616)

---
+++ Array/Binary Search/(Jun 13)_Minimize_the_Maximum_Difference_of_Pairs.cpp
@@ -0,0 +1,35 @@
+// LeetCode Problem No. 2616
+// Daily Challenge - 13/06/2025
+
+class Solution {
+    bool isPossible(vector<int>& nums,const int& val,const int& p){
+        int count = 0;
+        int i=1;
+        while(i<nums.size()){
+            if(nums[i]-nums[i-1] <= val){
+                count++;
+                i+=2;
+            }else{
+                i++;
+            }
+        }
+        return count>=p;
+    }
+    
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(),nums.end());
+        int low = 0, high = nums.back(), mid;
+        int ans = INT_MAX;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(isPossible(nums,mid,p)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
