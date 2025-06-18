afd318c0c3973c35e022a440c8fc711c4a50cf62
atharvkhardekar <khardekaratharv@gmail.com>
2025-06-18 23:38:40 +0530

    18/06/2025 - LeetCode(2966)

---
+++ Array/(Jun 18)_Divide_Array_Into_Arrays_With_Max_Difference.cpp
@@ -0,0 +1,29 @@
+// LeetCode Problem No. 2966
+// Daily Challenge - 18/06/2025
+
+class Solution
+{
+public:
+    vector<vector<int>> divideArray(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+
+        sort(begin(nums), end(nums));
+
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n; i += 3)
+        {
+            if (nums[i + 2] - nums[i] > k)
+            {
+                return {};
+            }
+            else
+            {
+                ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
+            }
+        }
+
+        return ans;
+    }
+};
