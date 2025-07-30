05981385bcdf12514d61bf451d09c73baf8ef1be
atharvkhardekar <khardekaratharv@gmail.com>
2025-07-30 23:07:50 +0530

    30/07/2025 - LeetCode(2419)

---
+++ Array/Bit Manipulation/(Jul 30)_Longest_Subarray_With_Maximum_Bitwise_AND.cpp
@@ -0,0 +1,23 @@
+// LeetCode Problem No. 2419
+// Daily Challenge - 30/07/2025
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int max_value = *max_element(nums.begin(),nums.end());
+        
+        int max_len = 0;
+        int curr_len = 0;
+        for(int i=0; i<n; ++i){
+            if(nums[i] == max_value){
+                curr_len++;
+                if(max_len < curr_len)
+                    max_len = curr_len;
+            }else{
+                curr_len = 0;
+            }
+        }
+        return max_len;
+    }
+};
