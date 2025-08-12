8c13bd2ed7647e9cbe8dd034a8d1e882872afa74
atharvkhardekar <khardekaratharv@gmail.com>
2025-08-12 23:00:28 +0530

    12/08/2025 - LeetCode(2787)

---
+++ DP/(Aug 12)_Ways_to_Express_an_Integer_as_Sum_of_Powers.cpp
@@ -0,0 +1,32 @@
+// LeetCode Problem No. 2787
+// Daily Challenge - 12/08/2025
+
+class Solution{
+public:
+    static const int MOD = 1'000'000'007;
+    int numberOfWays(int n, int x)
+    {
+       
+        vector<int> powers;
+        for (int i = 1;; ++i)
+        {
+            long long p = 1;
+            for (int k = 0; k < x; ++k)
+                p *= i;
+            if (p > n)
+                break;
+            powers.push_back((int)p);
+        }
+
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int p : powers)
+        {
+            for (int s = n; s >= p; --s)
+            {
+                dp[s] = (dp[s] + dp[s - p]) % MOD;
+            }
+        }
+        return (int)dp[n];
+    }
+};
