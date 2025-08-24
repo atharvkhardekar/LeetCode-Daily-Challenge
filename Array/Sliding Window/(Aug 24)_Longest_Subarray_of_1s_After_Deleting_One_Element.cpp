2be6d10903c62ee8f219f648fe56a7dc5476b8d1
atharvkhardekar <khardekaratharv@gmail.com>
2025-08-24 22:55:35 +0530

    24/08/2025 - LeetCode(1493)

---
+++ Array/Sliding Window/(Aug 24)_Longest_Subarray_of_1s_After_Deleting_One_Element.cpp
@@ -0,0 +1,21 @@
+// LeetCode Problem No. 1493
+// Daily Challenge - 24/08/2025
+
+class Solution{
+public:
+    int longestSubarray(vector<int> &nums)
+    {
+        const int n = nums.size();
+        int zeros = 0, ans = 0;
+        for (int l = 0, r = 0; r < n; r++)
+        {
+            zeros += (nums[r] == 0);
+            while (zeros > 1 && nums[l++] == 0)
+            {
+                zeros--;
+            }
+            ans = max(ans, r - l);
+        }
+        return ans;
+    }
+};
