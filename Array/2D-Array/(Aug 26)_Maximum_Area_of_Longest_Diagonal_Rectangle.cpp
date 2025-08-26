45d8f9d6c40e80db378db45820bfbecf3d8297e1
atharvkhardekar <khardekaratharv@gmail.com>
2025-08-26 23:32:33 +0530

    26/08/2025 - LeetCode(3000)

---
+++ Array/2D-Array/(Aug 26)_Maximum_Area_of_Longest_Diagonal_Rectangle.cpp
@@ -0,0 +1,22 @@
+// LeetCode Problem No. 3000
+// Daily Challenge - 26/08/2025
+
+class Solution{
+public:
+    int areaOfMaxDiagonal(vector<vector<int>> &dimensions)
+    {
+        int maxArea = 0, Mdia2 = 0;
+        for (auto &r : dimensions)
+        {
+            int w = r[0], h = r[1], dia2 = w * w + h * h;
+            if (Mdia2 < dia2)
+            {
+                Mdia2 = dia2;
+                maxArea = 0;
+            }
+            if (dia2 == Mdia2)
+                maxArea = max(maxArea, w * h);
+        }
+        return maxArea;
+    }
+};
