d7964bbd9d5fa0e1a15f94277656390ad6b019b4
atharvkhardekar <khardekaratharv@gmail.com>
2025-09-05 22:50:15 +0530

    05/09/2025 - LeetCode(2749)

---
+++ Enumeration/(Sep 5)_Minimum_Operations_to_Make_the_Integer_Zero.cpp
@@ -0,0 +1,21 @@
+// LeetCode Problem No. 2749
+// Daily Challenge - 05/09/2025
+
+class Solution{
+public:
+    int makeTheIntegerZero(int num1, int num2)
+    {
+        for (int t = 1; t <= 60; t++)
+        {
+            long long s = (long long)num1 - (long long)t * num2;
+            if (s < 0)
+                continue;
+            if (s < t)
+                continue;
+            int ones = __builtin_popcountll(s);
+            if (ones <= t)
+                return t;
+        }
+        return -1;
+    }
+};
