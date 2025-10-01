fc76ff32d03d063c45f771f7bb3bc74e268c1cb4
atharvkhardekar <khardekaratharv@gmail.com>
2025-10-01 23:28:19 +0530

    01/10/2025 - LeetCode(1518)

---
+++ Math/(Oct 1)_Water_Bottles.cpp
@@ -0,0 +1,17 @@
+// LeetCode Problem No. 1518
+// Daily Challenge - 01/10/2025
+
+class Solution{
+public:
+    static int exchange(int n, int k)
+    {
+        if (n < k)
+            return 0;
+        auto [n0, r] = div(n, k);
+        return n0 + exchange(n0 + r, k);
+    }
+    static int numWaterBottles(int n, int k)
+    {
+        return n + exchange(n, k);
+    }
+};
