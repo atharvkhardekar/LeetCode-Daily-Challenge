8a1c160acee295d52298feaf9e1a2f11f828c474
atharvkhardekar <khardekaratharv@gmail.com>
2025-10-30 23:43:50 +0530

    30/10/2025 - LeetCode(1526)

---
+++ Array/(Oct 30)_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array.cpp
@@ -0,0 +1,13 @@
+// LeetCode Problem No. 1526
+// Daily Challenge - 30/10/2025
+
+class Solution{
+public:
+    int minNumberOperations(vector<int> &target)
+    {
+        int count = target[0];
+        for (int i = 1; i < target.size(); i++)
+            count += max(target[i] - target[i - 1], 0);
+        return count;
+    }
+};
