33ade93625035f092e29e590c9b3ef81747a7b0c
atharvkhardekar <khardekaratharv@gmail.com>
2025-11-13 23:09:04 +0530

    13/11/2025 - LeetCode(3228)

---
+++ String/Greedy/(Nov 13)_Maximum_Number_of_Operations_to_Move_Ones_to_the_End.cpp
@@ -0,0 +1,26 @@
+// LeetCode Problem No. 3228
+// Daily Challenge - 13/11/2025
+
+class Solution{
+public:
+    int maxOperations(string s)
+    {
+        int operations = 0;
+        int ones_count = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '1')
+            {
+                ones_count++;
+            }
+            else if (i > 0 && s[i - 1] == '1')
+            {
+
+                operations += ones_count;
+            }
+        }
+
+        return operations;
+    }
+};
